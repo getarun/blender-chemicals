8e4aa79d765457d501d6442786c36cfe7fc5bc8b
Matriox <zimmer@jzo.de>
2016-01-29 21:15:56 +0100

    - Added 100, 110 surfaces for fcc metals - improved support for other metals - improved coloring: color will depend on stacking order 	- hexago:       top,fcc,hcp     => 3 colors 	- square|rect : top, in between => 2 colors - unlimited numbers of layers

--- structures/substrate_to_blender.py
+++ structures/substrate_to_blender.py
@@ -13,27 +13,50 @@ with open(os.path.join(PATH, "atoms.json")) as in_file:
 
 def draw_substrate():
     scale = 1             #scales atomic radius by scale - does not change lattice constant
-    a = 0.255            #lattice constant in nm
-    a1 = a * 3.20432         # ####3,240692139
+    a = 0.361             #lattice constant in nm
+    element = "Cu"
+    a1 = a * 3.20432      # ####3,240692139
+    hexagonal = True
+    square = False
+    rect = False
+    if hexagonal:       # 111
+        a1 *= sqrt(2)/2        
+        dx=a1*cos(30)*1.0115836        # *1.0115836 to get nearest neighbour to 0.255
+        dy=a1*sin(30)/sqrt(3)* 0.947583    #     ------------ " --------------
+        d = (a1/sqrt(3)) * scale    / 5 #  original layer spacing
+        do_square = False
+        different_stacking = 3
+    if square:          # 100
+        a1 *= sqrt(2)/2       
+        dx = a1 * 0.312079648       #   match correct spacing
+        dy = a1 * 0.312079648
+        d = (a1/sqrt(1)) * scale    / 5 #  original layer spacing
+        do_square = True
+        different_stacking = 2
+    if rect:            #110
+        a2 = a1        
+        a1 *= sqrt(2)/2    
+            
+        dx = a1 * 0.312079648       #   match correct spacing
+        dy = a2 * 0.312079648
+        d = (a1/sqrt(2)) * scale    / 5 #  original layer spacing
+        do_square = True
+        different_stacking = 2
 
-    dx=a1*cos(30)*1.0115836        # *1.0115836 to get nearest neighbour to 0.255
-    dy=a1*sin(30)/sqrt(3)* 0.947583    #     ------------ " --------------
-
-    d = (a1/sqrt(3)) * scale    / 7 #  original layer spacing
     overall_time = time.time()
 #Iteration index, width of substrate drawn
-    n = 50		  # resembles atoms along close packed direction of moire cell created when do_square= False
-    layers = 3            ### number of layers to draw ... >8gb(15Gb) for 3rd layer with n=50(120).. .
+
+    n = 10		  # resembles atoms along close packed direction of moire cell created when do_square= False
+    layers = 8            ### number of layers to draw ... >8gb(15Gb) for 3rd layer with n=50(120).. .
     smooth = True
-    join = False
+    join = True     #set to false if you want to check distances for debugging purposes
     link_to_scene = True
 
     verbose = True            # first level of verbose
-    verbose2 = False        # second level of verbose
+    verbose2 = True        # second level of verbose
 
-    do_square = False		#creates an square overlayer and does not follow close packed lines
     if do_square:
-        array_mod = True        # adds standart modifier which will duplicate the sheet in x-direction
+        array_mod = False        # adds standart modifier which will duplicate the sheet in x-direction
         array_mod_count = 2        # doubles the width, so it becomes square!
     else: array_mod = False
 	
@@ -42,80 +65,99 @@ def draw_substrate():
     bpy.ops.object.select_all(action='DESELECT')
     bpy.ops.mesh.primitive_uv_sphere_add()
     sphere = bpy.context.object
-    sphere.dimensions = [atom_data["Cu"]["radius"]* scale] * 3
+    sphere.dimensions = [atom_data[element]["radius"]* scale] * 3
+    for i in range(1,different_stacking+1):         #start labelling with "Cu"1
+        key = element + str(i)
+        if verbose2: print("Creating Material", key)
+        bpy.data.materials.new(name=key)
+        bpy.data.materials[key].diffuse_color = (pow(1*0.6,i), pow(0.638152*0.6,i), pow(0.252242*0.6,i))    #light brown
+#        bpy.data.materials[key].diffuse_color = atom_data[element]["color"]
+        bpy.data.materials[key].diffuse_intensity = 0.7 - 0.2*i
+        bpy.data.materials[key].specular_intensity = 0.2 - 0.1*i
 
-    key = "Cu1"
-    bpy.data.materials.new(name=key)
-    bpy.data.materials[key].diffuse_color = (1, 0.638152, 0.252242)    #light brown
-#    bpy.data.materials[key].diffuse_color = atom_data["Cu"]["color"]
-    bpy.data.materials[key].diffuse_intensity = 0.7
-    bpy.data.materials[key].specular_intensity = 0.2
-
-    key = "Cu2"
-    bpy.data.materials.new(name=key)
-    bpy.data.materials[key].diffuse_color = (0.281, 0.183, 0.076)  #darker brown
-    #bpy.data.materials[key].diffuse_color = atom_data["Cu"]["color"]
-    bpy.data.materials[key].diffuse_intensity = 0.5
-    bpy.data.materials[key].specular_intensity = 0
-    bpy.data.materials[key].use_shadeless = False
-
-    key = "Cu3"
-    bpy.data.materials.new(name=key)
-    bpy.data.materials[key].diffuse_color = (0.073, 0.049, 0.022)  #dark brown
-#    bpy.data.materials[key].diffuse_color = atom_data["Cu"]["color"]
-    bpy.data.materials[key].diffuse_intensity = 0.2
-    bpy.data.materials[key].specular_intensity = 0
-    bpy.data.materials[key].use_shadeless = False
 ###############################################################################################
     shapes = []
 # build first layer
     print("Drawing layer 1:")
     layer_start_time = time.time()
-    if do_square: 
+
+    if rect: 
         for i in range(n):
+            row_start_time = time.time()
             for j in range(n):
-                if verbose2:
-                    print("row: ", j, " of ", n)
-                row_start_time = time.time()
-                atom_sphere = sphere.copy()
-                atom_sphere.data = sphere.data.copy()
-                atom_sphere.location = (2*i*dx,j*dy,0)
-                atom_sphere.active_material = bpy.data.materials["Cu1"]
-                bpy.context.scene.objects.link(atom_sphere)
-                shapes.append(atom_sphere)
+                if verbose2: print("Atom: ", j, " of ", n)
 
                 atom_sphere = sphere.copy()
                 atom_sphere.data = sphere.data.copy()
-                atom_sphere.location = (2*i*dx+dx,j*dy+dy/2,0)
-                atom_sphere.active_material = bpy.data.materials["Cu1"]
+                atom_sphere.location = (i*dx,j*dy,0)
+                atom_sphere.active_material = bpy.data.materials[element + "1"]
                 bpy.context.scene.objects.link(atom_sphere)
                 shapes.append(atom_sphere)
-                if verbose2: print("Atom: ",j)
+
+            row_run_time = round((time.time() - row_start_time),2)
+            if verbose: print("Time for row: ", i, " in layer I is ", row_run_time)
+    
+
+    if square: 
+        for i in range(n):
+            row_start_time = time.time()
+            for j in range(n):
+                if verbose2: print("Atom: ", j, " of ", n)
+
+                atom_sphere = sphere.copy()
+                atom_sphere.data = sphere.data.copy()
+                atom_sphere.location = (i*dx,j*dy,0)
+                atom_sphere.active_material = bpy.data.materials[element + "1"]
+                bpy.context.scene.objects.link(atom_sphere)
+                shapes.append(atom_sphere)
+
             row_run_time = round((time.time() - row_start_time),2)
             if verbose: print("Time for row: ", i, " in layer I is ", row_run_time)
 			
 				
-    else: 
-        for j in range(0,int(n/2)+1,1):					# for j in range(0,int(n/2),1): raute along close packed row
-            if verbose2: print("row: ", i, " of ", n)
-            row_start_time = time.time()
-            for i in range(-j,(n-j)+2,1):				# for i in range(-j,n-j,1): raute along close packed row
-                atom_sphere = sphere.copy()
-                atom_sphere.data = sphere.data.copy()
-                atom_sphere.location = (2*i*dx,j*dy,0)
-                atom_sphere.active_material = bpy.data.materials["Cu1"]
-                bpy.context.scene.objects.link(atom_sphere)
-                shapes.append(atom_sphere)
+    if hexagonal: 
+        if do_square:
+            for i in range(n):
+                row_start_time = time.time()
+                for j in range(n):
+                    if verbose2: print("Atom: ", j, " of ", n)
+
+                    atom_sphere = sphere.copy()
+                    atom_sphere.data = sphere.data.copy()
+                    atom_sphere.location = (2*i*dx,j*dy,0)
+                    atom_sphere.active_material = bpy.data.materials[element + "1"]
+                    bpy.context.scene.objects.link(atom_sphere)
+                    shapes.append(atom_sphere)
+
+                    atom_sphere = sphere.copy()
+                    atom_sphere.data = sphere.data.copy()
+                    atom_sphere.location = (2*i*dx+dx,j*dy+dy/2,0)
+                    atom_sphere.active_material = bpy.data.materials[element + "1"]
+                    bpy.context.scene.objects.link(atom_sphere)
+                    shapes.append(atom_sphere)
+                row_run_time = round((time.time() - row_start_time),2)
+                if verbose: print("Time for row: ", j, " in layer I is ", row_run_time)
+        else:
+            for j in range(0,int(n/2)+1,1):					# for j in range(0,int(n/2),1): raute along close packed row
+                if verbose2: print("row: ", i, " of ", n)
+                row_start_time = time.time()
+                for i in range(-j,(n-j)+2,1):				# for i in range(-j,n-j,1): raute along close packed row
+                    atom_sphere = sphere.copy()
+                    atom_sphere.data = sphere.data.copy()
+                    atom_sphere.location = (2*i*dx,j*dy,0)
+                    atom_sphere.active_material = bpy.data.materials[element + "1"]
+                    bpy.context.scene.objects.link(atom_sphere)
+                    shapes.append(atom_sphere)
 	
-                atom_sphere = sphere.copy()
-                atom_sphere.data = sphere.data.copy()
-                atom_sphere.location = (2*i*dx+dx,j*dy+dy/2,0)
-                atom_sphere.active_material = bpy.data.materials["Cu1"]
-                bpy.context.scene.objects.link(atom_sphere)
-                shapes.append(atom_sphere)
-                if verbose2: print("Atom: ",i)
-            row_run_time = round((time.time() - row_start_time),2)
-            if verbose: print("Time for row: ", j, " in layer I is ", row_run_time)
+                    atom_sphere = sphere.copy()
+                    atom_sphere.data = sphere.data.copy()
+                    atom_sphere.location = (2*i*dx+dx,j*dy+dy/2,0)
+                    atom_sphere.active_material = bpy.data.materials[element + "1"]
+                    bpy.context.scene.objects.link(atom_sphere)
+                    shapes.append(atom_sphere)
+                    if verbose2: print("Atom: ",i)
+                row_run_time = round((time.time() - row_start_time),2)
+                if verbose: print("Time for row: ", j, " in layer I is ", row_run_time)
 
     layer_runtime = round((time.time() - layer_start_time),2)
     print("--- Runtime: ", layer_runtime, " seconds --- for first layer")
@@ -124,13 +166,17 @@ def draw_substrate():
     for shape in shapes:
         shape.select = True
     bpy.context.scene.objects.active = shapes[0]    #do not indent in for-selection loop
-    print("Joining into single Layer...")
-    start_time = time.time()
-    bpy.ops.object.join()                #do not indent in for-selection loop
-    if verbose: print("Time for joining: ", round((time.time() - start_time),2))
-    if verbose: print("")
-    bpy.ops.object.select_all(action='SELECT')    # select all objects (clean scene before?!)
+    if join:
+        print("Joining into single Layer...")
+        start_time = time.time()
+        bpy.ops.object.join()                #do not indent in for-selection loop
+        if verbose: print("Time for joining: ", round((time.time() - start_time),2))
+        if verbose: print("")
+    bpy.ops.object.select_all(action='SELECT')    # select all objects (cleaned scene before?!)
 ##########################
+# the following only works if join = True
+    if not join: 
+        print("!!!!!!!!!enable join to build several layers!!!!!!!!!")
     layer = bpy.context.object
     layer.name = "Layer 1"
     apply_list = []            # schreibt ertzeugte layer mit, damit man sie einzeln anwählen kann
@@ -144,22 +190,43 @@ def draw_substrate():
         if verbose:
             print("Time for copying: ", round((time.time() - start_time),2))
 
-        print("copying layer data")
-        next_layer.data = layer.data.copy()            #check if needed
-        print("move layer by ",i,"*dx, ",i,"*dy/6, -",i,"*d")  #this has to be proper to apply correct array_modifier later on
-        if i%3 == 0: next_layer.location += Vector((0, 0, -i*d))  # stack everyx 3rd above the first (fcc)
-        if i%3 == 1: next_layer.location += Vector((1*dx, 1*dy/6, -i*d))
-        if i%3 == 2: next_layer.location += Vector((2*dx, 2*dy/6, -i*d))
+        if hexagonal:
+            print("copying layer data")
+            next_layer.data = layer.data.copy()            #check if needed
+            if i%3 == 0: 
+                next_layer.location += Vector((0, 0, -i*d))  
+            if i%3 == 1: 
+                next_layer.location += Vector((2*dx, 2*dy/6, -i*d))
+            if i%3 == 2: 
+                next_layer.location += Vector((1*dx, 1*dy/6, -i*d)) #first stacked layer, i.e.1%3=2
+               
+            print("Change material")
+            materialname = element + str(i%3+1)   # Layer1 already created out of single atoms, start at 2
+     #       if i > 2:
+     #           materialname = "Cu3"        # use same color for layers > 3 (not visible from above)
+            layername = "Layer " + str(i+1)
+            next_layer.name = layername
+            next_layer.active_material = bpy.data.materials[materialname]
+            bpy.context.scene.objects.link(next_layer)
 
-        print("Change material")
-        materialname = "Cu" + str(i+1)
-        if i > 2:
-            materialname = "Cu3"        # use same color for layers > 3 (not visible from above)
-        layername = "Layer " + str(i+1)
-        next_layer.name = layername
-        next_layer.active_material = bpy.data.materials[materialname]
-        bpy.context.scene.objects.link(next_layer)
-            
+        if square|rect:
+            print("copying layer data")
+            next_layer.data = layer.data.copy()            #check if needed
+            if i%2 == 0: 
+                next_layer.location += Vector((0, 0, -i*d))  
+            if i%2 == 1: 
+                next_layer.location += Vector((dx/2, dy/2, -i*d))
+               
+            print("Change material")
+            materialname = element + str(i%2+1)   # Layer1 already created out of single atoms, start at 2
+     #       if i > 2:
+     #           materialname = "Cu3"        # use same color for layers > 3 (not visible from above)
+            layername = "Layer " + str(i+1)
+            next_layer.name = layername
+            next_layer.active_material = bpy.data.materials[materialname]
+            bpy.context.scene.objects.link(next_layer)
+        
+#################            
         if verbose: print("Add Layers to apply_list")
         apply_list.append(layername)
 
@@ -205,45 +272,6 @@ def draw_substrate():
             print("")
 
     print("Overall time: ",round((time.time() - overall_time),2))
-
-#        for item in apply_list:            #Liste aller Layer die in der Schleife erzeugt wurden
-#            layer_time = time.time()
-#            if item in bpy.data.objects.keys():
-#                if verbose: print("Found "+item+" in list of object-keys")
-#                if verbose: print("Deselecting all")
-#                bpy.ops.object.select_all(action='DESELECT')    # deselect all objects
-#                if verbose: print("Selecting ", item)
-#                bpy.data.objects.get(item).select = True
-##            bpy.context.scene.objects.active = shape[0]
-#            if verbose: print("Addind modifier 'ARRAY' ")
-#            bpy.ops.object.modifier_add(type='ARRAY')
-#            bpy.context.object.modifiers["Array"].count = array_mod_count
-#            if verbose: print("Applying modifier 'ARRAY' ")
-#            bpy.ops.object.modifier_apply(apply_as = 'DATA', modifier="Array")
-#            if verbose:
-#                print("Time for applying modifier on ", item , round((time.time() - layer_time),2))
-#                print("")
-#
-#        if verbose:
-#            print("Total time for applying modifier: ", round((time.time() - start_time),2))
-
-##########################
-
-#    # copy first layer into second one
-#    print("copying layer 1")
-#    layer = bpy.context.object
-#    layer2 = layer.copy()
-#    layer2.data = layer.data.copy()            #check if needed
-#    layer2.location += Vector((dx, dy/6, -d))
-#    layer2.active_material = bpy.data.materials["Cu2"]
-#    bpy.context.scene.objects.link(layer2)
-#    # copy second layer into third one
-#    print("copying layer 2")
-#    layer3 = layer2.copy()
-#    layer3.data = layer2.data.copy()        #check if needed
-#    layer3.location += Vector((dx, dy/6, -d))
-#    layer3.active_material = bpy.data.materials["Cu3"]
-#    bpy.context.scene.objects.link(layer3)
 
 def clear_scene():
 # If the starting cube is there, remove it
